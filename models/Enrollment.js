38510f2ca9348ac4482ccc6b1d50b960d9a9c3d0
Padmini <vtu24646@veltech.edu.in>
2026-03-16 11:28:47 +0530

    Delete models directory

--- models/Enrollment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const EnrollmentSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Enrollment', EnrollmentSchema);
